662db5174a6e089c3b3ae083db97cc3543c7c09b
salmansadafi <sadafitehran.ms@gmail.com>
2025-02-05 16:28:48 +0330

    created first app
    created first app named "website" with home, about, contact pages

--- mysite/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse,JsonResponse
-def http_test(request):
-    return HttpResponse("<h1>http-test</h1>")
-
-def json_test(request):
-    return JsonResponse({"name":"zhangsan","age":18})
