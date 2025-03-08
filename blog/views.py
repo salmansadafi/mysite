73fc2919bb71c4f38ccf7fb4b60fee58e5c68db9
salmansadafi <sadafitehran.ms@gmail.com>
2025-03-08 16:26:26 +0330

    show data in website
    showing data from database in website

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts=Post.objects.filter(status=1)
+    context={'posts':posts} 
+    return render(request, 'blog/blog-home.html',context)
 
 def blog_single_view(request):
     context = {'title':'bitcoin price','content':'a content for a test','author':'salmansadafi'}
     return render(request, 'blog/blog-single.html',context)
+
+def test(request):
+    posts=Post.objects.all()
+    context={'posts':posts} 
+    return render(request, 'test.html',context)
