d008e798e2028563b05936843827f0485d68c2a2
salmansadafi <sadafitehran.ms@gmail.com>
2025-03-13 23:23:45 +0330

    add url to posts
    add dynamic url to posts
    delete tests

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,6 @@ from blog.views import *
 app_name="blog"
 urlpatterns = [
     path('',blog_view,name="index"),
-    path('single',blog_single_view,name="single"),
-    path('post-<int:pid>',test,name="test"),
-    path('<int:post_id>',blog_detail,name="detail"),
+    path('<int:pid>',blog_single_view,name="single"),
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,24 +4,15 @@ from django.utils import timezone
 
 # Create your views here.
 def blog_view(request):
-    posts=Post.objects.filter(published_date__lte=timezone.now())
+    posts=Post.objects.filter(status=1,published_date__lte=timezone.now())
     context={'posts':posts} 
     return render(request, 'blog/blog-home.html',context)
 
-def blog_detail(request, post_id):
-    post = get_object_or_404(Post, id=post_id, published_date__lte=timezone.now())
 
+def blog_single_view(request,pid):
+    post = get_object_or_404(Post, id=pid,status=1 ,published_date__lte=timezone.now())
     post.counted_views += 1
     post.save(update_fields=['counted_views'])
-    context={'post':post}
-    return render(request, 'test.html',context)
-
-
-def blog_single_view(request):
-    context = {'title':'bitcoin price','content':'a content for a test','author':'salmansadafi'}
+    context = {'post':post}
     return render(request, 'blog/blog-single.html',context)
 
-def test(request,pid):
-    post=get_object_or_404(Post, id=pid)
-    context={'post':post} 
-    return render(request, 'test.html',context)
