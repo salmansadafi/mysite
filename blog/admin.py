a596348726516803bfa88242428ad01ebf1b6434
salmansadafi <sadafitehran.ms@gmail.com>
2025-03-16 17:30:15 +0330

    add category field

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post,Category
 # Register your models here.
 
-@admin.register(Post)  # Decorator to register the Post model with the admin site
+#@admin.register(Post)  # Decorator to register the Post model with the admin site
+
+
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy ="created_date"  # Adds a date-based drill-down navigation in the admin list view
     empty_value_display = "-empty-"  # Customizes the display of empty values in the admin list view
     #fields = ('title',)  # Specifies the fields to display in the form view for the Post model
-    list_display = ('title', 'status','author', 'counted_views', 'created_date' , 'published_date')  # Specifies the fields to display in the list view for the Post model
+    list_display = ('title','status','author', 'counted_views', 'created_date' , 'published_date')  # Specifies the fields to display in the list view for the Post model
     list_filter =('status',)  # Adds a filter sidebar to the admin list view. حتما باید نوعش تاپل باشه یعنی اخرش کاما باشه
     search_fields = ['title', 'content']  # Adds a search box to the admin list view
-#admin.site.register(Post,PostAdmin) --> @admin.register(Post)
+
+admin.site.register(Post,PostAdmin) #--> @admin.register(Post)
+admin.site.register(Category)
