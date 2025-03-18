67dc479065ccc7d40e40d4a866567dda3c32c4b2
salmansadafi <sadafitehran.ms@gmail.com>
2025-03-18 16:37:48 +0330

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -13,8 +13,8 @@ def blog_single_view(request,pid):
     post = get_object_or_404(Post, id=pid,status=1 ,published_date__lte=timezone.now())
     post.counted_views += 1
     post.save(update_fields=['counted_views'])
-    prev_post = Post.objects.filter(status=1,id__lt=post.id).order_by('id').last()
-    next_post = Post.objects.filter(status=1,id__gt=post.id).order_by('id').first()
+    prev_post = Post.objects.filter(status=1,published_date__lte=timezone.now(),id__lt=post.id).order_by('id').last()
+    next_post = Post.objects.filter(status=1,published_date__lte=timezone.now(),id__gt=post.id).order_by('id').first()
     context = {
         'post':post,
         'prev_post':prev_post,
