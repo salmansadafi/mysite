56fce2bab4dc51dbfdadb49455d18b89f788b012
salmansadafi <sadafitehran.ms@gmail.com>
2025-03-21 17:23:37 +0330

    create template tags
    set latest posts by template tags

--- blog/templatetags/blog-tags.py
+++ blog/templatetags/blog-tags.py
@@ -1,3 +1,10 @@
 from django import template
-
+from blog.models import Post
+import django.utils.timezone as timezone
 register = template.Library()
+
+
+@register.inclusion_tag('blog/blog-popular-post.html')
+def latest_post(arg=3):
+    posts=Post.objects.filter(status=1,published_date__lte=timezone.now()).order_by('-published_date')[:arg]
+    return {'posts':posts}
