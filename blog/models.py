cd40a46e81ff6f734bb47dda8b9a24eabed34531
salmansadafi <sadafitehran.ms@gmail.com>
2025-04-02 15:13:12 +0330

    add sitemap
    we can see sitemap with search :
    127.0.0.1:8000/sitemap.xml

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+from django.urls import reverse
 # Create your models here.
 
 class Category(models.Model):
@@ -26,4 +28,7 @@ class Post(models.Model):
 
     def __str__(self):
         return "{} - {}".format(self.title, self.content)
+    
+    def get_absolute_url(self):
+        return reverse("blog:single", kwargs={"pid": self.id})
     
--- mysite/settings.py
+++ mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'django_extensions',
     'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 SITE_ID = 2
