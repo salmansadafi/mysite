a510d828d325b027b0288a9a27fbe56b2fac3675
salmansadafi <sadafitehran.ms@gmail.com>
2025-04-05 17:01:07 +0330

    authentication with form

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
@@ -7,14 +8,23 @@ def signup_view(request):
     return render(request,'accounts/signup.html')
 
 def login_view(request):
-    if request.method == 'POST':
-        username=request.POST.get('username')
-        password=request.POST.get('password')
-        user=authenticate(request,username=username,password=password)
-        if user is not None:
-            login(request,user)
-            return redirect('/')
-    return render(request,'accounts/login.html')
+    if request.user.is_anonymous:
+        if request.method == 'POST':
+            form=AuthenticationForm(request,data=request.POST)
+            if form.is_valid():
+                username=form.cleaned_data.get('username')
+                password=form.cleaned_data.get('password')
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            user=authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('/')
+        form=AuthenticationForm()
+        context={'form':form}
+        return render(request,'accounts/login.html',context)
+    else:
+        return redirect('/')
 
 # def logout_view(request):
 #     return render(request,'accounts/logout.html')
