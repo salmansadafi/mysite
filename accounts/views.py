6af0f4149a95dc0e2ac9c6ba1726e4ea918452c5
salmansadafi <sadafitehran.ms@gmail.com>
2025-04-05 15:33:06 +0330

    complete login page
    download a login template

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
 
-def signup(request):
+def signup_view(request):
     return render(request,'accounts/signup.html')
 
-def login(request):
+def login_view(request):
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
     return render(request,'accounts/login.html')
 
-# def logout(request):
+# def logout_view(request):
 #     return render(request,'accounts/logout.html')
