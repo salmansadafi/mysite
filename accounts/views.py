c908386b748a6a0bde03f40712e1f01eb73c351a
salmansadafi <sadafitehran.ms@gmail.com>
2025-04-06 22:07:49 +0330

    change username field to username or email

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db.models import Q
 
 # Create your views here.
 
@@ -22,10 +24,15 @@ def login_view(request):
         if request.method == 'POST':
             form=AuthenticationForm(request,data=request.POST)
             if form.is_valid():
-                username=form.cleaned_data.get('username')
+                username_or_email=form.cleaned_data.get('username')
                 password=form.cleaned_data.get('password')
-            username=request.POST.get('username')
+            username_or_email=request.POST.get('username')
             password=request.POST.get('password')
+            try:
+                user_obj = User.objects.get(Q(username=username_or_email) | Q(email=username_or_email))
+                username = user_obj.username  # دریافت یوزرنیم معادل ایمیل
+            except User.DoesNotExist:
+                username = username_or_email
             user=authenticate(request,username=username,password=password)
             if user is not None:
                 login(request,user)
