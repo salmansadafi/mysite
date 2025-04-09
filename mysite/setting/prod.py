41111c756418b473a60197208ed207281cf7c302
salmansadafi <sadafitehran.ms@gmail.com>
2025-04-09 11:43:44 +0330

    start launch site on a host
    create setting file and dev.py and prod.py

---
+++ mysite/setting/prod.py
@@ -0,0 +1,32 @@
+from mysite.settings import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-v-#0e6(@123=kkn-ctw$*&#ib3q4vcqpzl5+t23p8xtfrkyi!_'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+# sites framework
+SITE_ID = 2
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_ROOT=BASE_DIR/'static'
+MEDIA_ROOT=BASE_DIR/'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'statics',
+]
--- mysite/settings.py
+++ mysite/settings.py
@@ -16,16 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v-#0e6(@123=kkn-ctw$*&#ib3q4vcqpzl5+t23p8xtfrkyi!_'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -52,8 +43,7 @@ INSTALLED_APPS = [
     'django_summernote',
     'captcha',
 ]
-# sites framework
-SITE_ID = 2
+
 
 # robots
 ROBOTS_USE_HOST = True
@@ -97,15 +87,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -143,14 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=BASE_DIR/'static'
-
 MEDIA_URL = 'media/'
-MEDIA_ROOT=BASE_DIR/'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'statics',
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
