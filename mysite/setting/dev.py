73e46c4e7de0f59ee82698e52dd1456c5422f111
salmansadafi <sadafitehran.ms@gmail.com>
2025-04-11 21:19:08 +0330

    liara1

--- mysite/setting/dev.py
+++ mysite/setting/dev.py
@@ -18,10 +18,7 @@ SITE_ID = 2
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')),
 }
 
 STATIC_ROOT=BASE_DIR/'static'
